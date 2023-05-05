15b48fc16410536f28eb0aca96857fe7505ec9b4
g54 <giftisacc54@gmail.com>
2023-05-05 13:58:49 +0300

    update

--- app/src/main/java/com/example/storage/MainActivity.java
+++ app/src/main/java/com/example/storage/MainActivity.java
@@ -4,16 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String darkTheme = "Dark";
+    public static final String lightTheme = "Light";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSavedTheme();
+
     }
 
 
@@ -27,12 +33,44 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.btn_theme_dark:
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                changeTheme(lightTheme);
+                savaTheme(lightTheme);
                 return true;
             case R.id.btn_theme_light:
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                changeTheme(darkTheme);
+                savaTheme(darkTheme);
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void changeTheme(String theme){
+        switch (theme){
+            case "Dark":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            case "Light":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+            case "Default":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
+        }
+    }
+    public void savaTheme(String theme){
+        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("theme",theme);
+        editor.apply();
+    }
+
+    public String getSavedTheme(){
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        return sharedPref.getString("theme", "Default");
+    }
+
+    public void setSavedTheme(){
+        changeTheme(getSavedTheme());
     }
 }
